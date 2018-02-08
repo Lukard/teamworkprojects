425320965cff00abe2da9d5c78944ef025699035
Lukard <rubenabad89@gmail.com>
2018-02-08 06:42:46 +0100

    Added first ViewModel to retrieve projects

---
+++ app/src/main/java/com/rubenabad/teamworkprojects/viewmodel/ProjectListViewModel.kt
@@ -0,0 +1,21 @@
+package com.rubenabad.teamworkprojects.viewmodel
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import com.rubenabad.teamworkprojects.data.Project
+import com.rubenabad.teamworkprojects.repository.ProjectsRepository
+import io.reactivex.schedulers.Schedulers
+
+class ProjectListViewModel(private val repository: ProjectsRepository) : ViewModel() {
+
+    private val projects = MutableLiveData<List<Project>>()
+
+    fun getAllProjects(): LiveData<List<Project>> = projects.apply {
+        repository
+                .getProjects()
+                .subscribeOn(Schedulers.io())
+                .subscribe(this::postValue)
+    }
+
+}
